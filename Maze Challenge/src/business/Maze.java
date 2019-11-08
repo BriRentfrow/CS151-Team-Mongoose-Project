9c31c6b3f26864dda51ee9b653e826a3d03317b8
Theboyg <57510324+Theboyg@users.noreply.github.com>
2019-11-07 17:15:19 -0800

    Update Maze.java

--- Maze Challenge/src/business/Maze.java
+++ Maze Challenge/src/business/Maze.java
@@ -10,7 +10,38 @@ public class Maze extends Model{
    private   int playerCol;
    public static int SIZE = 20;
 
+
+
+    public int distanceToExit() //theorem
+    {
+        //get player position
+        //get exit position
+        //drop decimal value
+        int x = playerPosX - exitX;
+        x = xx;
+        int y = playerPosY - exitY;
+        y = yy;
+        double function = Math.sqrt(x + y);
+        double rounded = Math.round(function); //decimal is .0
+        return (int) rounded;
+    }
+
+    public int distanceToExit() //pothagreum theorem
+    {
+        //get player position
+        //get exit position
+        //drop decimal value
+        int x = playerPosX - exitX;
+        x = xx;
+        int y = playerPosY - exitY;
+        y = yy;
+        double function = Math.sqrt(x + y);
+        double rounded = Math.round(function); //decimal is .0
+        return (int) rounded;
+
 }
+
+
  public class enum Heading{
 
 
