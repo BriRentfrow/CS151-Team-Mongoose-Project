42bbe28955168d806bd746246a6b3978a8eb3259
Theboyg <57510324+Theboyg@users.noreply.github.com>
2019-11-07 17:05:49 -0800

    Update Maze.java

--- Maze Challenge/src/business/Maze.java
+++ Maze Challenge/src/business/Maze.java
@@ -3,6 +3,15 @@ package business;
 import framework.Model;
 
 public class Maze extends Model{
-    // test
+   private  int movesLeft = 20;
+   private  int exitRow;
+   private  int exitCol;
+   private  int playerRow;
+   private   int playerCol;
+   public static int SIZE = 20;
+
+ public class enum Heading{
+
+    
 
 }
